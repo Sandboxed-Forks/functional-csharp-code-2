3c458f2fb66c19bcb4dec720ebf8c50f17869b84
Dan Gilleland <dagilleland@shaw.ca>
2022-12-31 02:09:53 -0700

    Reopen: Exercise.2.1 - WIP
    Also added FluentAssertions
    
    After reviewing the solution, I see I misunderstood the original question.
    
    WIP: Need to think through how I would use with a Predicate<in T> delegate

--- Exercises/Chapter02/Exercises.cs
+++ Exercises/Chapter02/Exercises.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace Exercises.Chapter2
 {
@@ -8,7 +10,35 @@ namespace Exercises.Chapter2
    {
       // 1. Write a function that negates a given predicate: whenever the given predicate
       // evaluates to `true`, the resulting function evaluates to `false`, and vice versa.
-      static bool Negate(bool value) => !value;
+      //    Clarification: Negating a predicate, where a "predicate" is a delegate that
+      //                   returns a bool upon receiving some <T> item passed in.
+      //                      public delegate bool Predicate<in T>(T obj);
+      //                   
+      static Func<T, bool> Negate<T>(this Func<T, bool> self) => t => !self(t);
+      //     ^-----------^                                       ^-----------^
+      //                                                           |_ 
+
+      [Test] public static void TestNegate()
+      {
+         Func<int, bool> divBy3 = x => x%3 == 0;
+         var actual = divBy3.Negate();
+         actual(6).Should().BeFalse();
+         // Predicate<int> givenDivisibleByThree = (x) => x % 3 == 0;
+         // bool actual = givenDivisibleByThree.Negate(6);
+         // actual.Should().BeTrue();
+      }
+
+      static bool Negate<T>(this Predicate<T> self)
+      {
+         return false;
+      }
+
+      [Test] public static void TestNegatePredicate()
+      {
+         Predicate<int> givenDivisibleByThree = (x) => x % 3 == 0;
+         bool actual = givenDivisibleByThree.Negate();
+         // actual.Should().BeTrue();
+      }
 
       // 2. Write a method that uses quicksort to sort a `List<int>` (return a new list,
       // rather than sorting it in place).
